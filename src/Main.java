9e7d71e879088a68edf79b3cc2c24f5232f19586
Sasha <slanecru@outlook.com>
2022-08-13 22:53:27 +0300

    Completed homework 1.9

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author firstAuthor = new Author("Kathy", "Sierra");
+        Author secondAuthor = new Author("Bruce", "Eckel");
+
+        Book firstBook = new Book("Head First Java", firstAuthor, 2012);
+        Book secondBook = new Book("Thinking in Java", secondAuthor, 2015);
+
+        System.out.println("Author : " + firstAuthor.getFullName() + " book: " + firstBook.getBookName() + " " +
+                firstBook.getYear() + " year.");
+        System.out.println("Author : " + secondAuthor.getFullName() + " book: " + secondBook.getBookName() + " " +
+                secondBook.getYear() + " year.");
     }
 }
